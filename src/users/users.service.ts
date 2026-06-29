03a654cf5ff39bf2556670a212869827ddf52665
2gab <info@2gabriel.com>
2026-06-29 11:57:56 -0300

    feat: migrate database from postgresql to sqlite

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,12 @@
 import type { CreateUserRequest } from "./users.types.js";
+import { prisma } from "../lib/prisma.js";
 
 type CreateUserBody = CreateUserRequest["Body"];
 
 export async function createUserService(data: CreateUserBody) {
-    return data;
+    const user = await prisma.user.create({
+        data,
+    });
+
+    return user;
 }
