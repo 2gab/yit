298ce902c04c34ababa83082c14bed7055e79e35
2gab <info@2gabriel.com>
2026-06-29 12:30:10 -0300

    feat: hash passwords with argon2 and omit password field from response

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,17 +1,20 @@
 import type { z } from "zod";
 import type { createUserSchema } from "./users.schema.js";
 import { prisma } from "../lib/prisma.js";
+import * as argon2 from "argon2";
 
 type CreateUserBody = z.infer<typeof createUserSchema.body>;
 
 export async function createUserService(data: CreateUserBody) {
-    const user = await prisma.user.create({
-        data: {
-            email: data.email,
-            password: data.password,
-            name: data.name ?? null,
-        },
-    });
+  const hashedPassword = await argon2.hash(data.password);
 
-    return user;
+  const { password: _, ...user } = await prisma.user.create({
+    data: {
+      email: data.email,
+      password: hashedPassword,
+      name: data.name ?? null,
+    },
+  });
+
+  return user;
 }
