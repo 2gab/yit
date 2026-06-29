333e650eefdd817617164265ea3d9f1a80bc4ce7
2gab <info@2gabriel.com>
2026-06-29 11:19:34 -0300

    fix: use import type for type-only imports from fastify

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { FastifyReply, FastifyRequest } from "fastify";
+import type { FastifyReply, FastifyRequest } from "fastify";
 import type { CreateUserRequest } from "./users.types.js";
 import { createUserService } from "./users.service.js";
 
--- src/users/users.routes.ts
+++ src/users/users.routes.ts
@@ -1,4 +1,4 @@
-import { FastifyInstance } from "fastify";
+import type { FastifyInstance } from "fastify";
 import { createUserController } from "./users.controller.js";
 
 export async function usersRoutes(app: FastifyInstance) {
